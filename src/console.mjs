a8efa38f2892d187845265932939fb5abdb7c0cb
LenAnderson <anderson.len.ja@gmail.com>
2025-02-08 17:20:41 -0400

    error to console format

--- src/console.mjs
+++ src/console.mjs
@@ -1,3 +1,3 @@
 export const log = (...msgs)=>console.log('[ChatSearch]', ...msgs);
 export const warn = (...msgs)=>console.warn('[ChatSearch]', ...msgs);
-export const error = (...msgs)=>console.error('[ChatSearch]', ...msgs);
+export const error = (...msgs)=>console.log('[ChatSearch]', '[ERROR]', ...msgs);
